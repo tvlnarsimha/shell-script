ce30b53962d09a8c478e7f90f9153f905924d22a
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-19 14:41:39 +0530

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#declaring variable
 PERSON1=Sachin
 PERSON2=Rahul
-echo "$PERSON1:: Hi Ramesh, I am fine.How are you?"
+
+#referring variable
+echo "$PERSON1:: Hello $PERSON2 How are you "
+echo "$PERSON1:: Hi $PERSON1, I am fine.How are you?"
 echo "$PERSON2: I am fine too. How is your work"
 echo "$PERSON1:: not bad. I am thinking to upgrade to Devops" 
