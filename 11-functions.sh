b06905868c2920c85ab5600a9a28dc1fd49c97ad
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-20 14:32:19 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,13 +2,13 @@
 
 USERID=$(id -u)
 VALIDATE() {
-    if [$1 ne 0 ]
+    if [$1 -ne 0 ]
     then 
       echo "$2...Failure"
       exit 1
     else
      echo "$2...success"
-
+    fi
 }
 
 if [ $USERID -ne 0 ]
