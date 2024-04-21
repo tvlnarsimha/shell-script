854415c72bcc2f98164709b9cb8ff1c0fcaa1936
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-21 16:18:27 +0530

    functions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Sachin
+PERSON2=Rahul
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1: I am fine too. how is your work?"
