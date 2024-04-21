2d860f3be1bca7bccd3840dfbf9a25bf08d8b5fe
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-21 18:18:50 +0530

    functions

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
  echo "Please run this script with root access"
- exit 0 # manually exit if error comes
+ exit  # manually exit if error comes
 else
  echo "you are the super user"
 fi
