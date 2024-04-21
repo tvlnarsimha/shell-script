5ecfd04ff7756f0c47a12109382d0046102d3038
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-21 16:52:40 +0530

    functions

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter the user name::"
+
+read USERNAME #HERE USERNAME IS VARIABLE
+
+echo "Please enter the password::"
+
+read PASSWORD
+
+echo "username is :$USERNAME,Password is :$PASSWORD"
+
