256e20f0f0c7c1a08b92ee40e28152916b206cd9
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-21 16:17:23 +0530

    functions

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 
 PERSON1=Ramesh
 PERSON2=Suresh
-echo "$PERSON1:: Hello PERSON2, How are you?"
-echo "PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1: I am fine too. how is your work?"
-echo "PERSON2:: not bad. I am thinking to upgrade to DevOps"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
