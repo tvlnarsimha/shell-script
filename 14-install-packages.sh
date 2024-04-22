f8332335d0158360a58a2b003c1c922e07ad403e
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-22 11:47:00 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+    then
+      echo "$2 ... Failure"
+      exit 1
+    else
+     echo "$2...Success"
+   fi
+}
 if [ $USERID -ne 0 ]
 then
  echo "Please run this script with root access"
@@ -14,5 +27,14 @@ echo "All packages :$@"
 for i in $@
 do
   echo "package to install: $i"
-  dnf list installed $i
+  dnf list installed $i &>>$LOGFILE
+  if [ $? -eq 0 ]
+  then
+   echo "$i already installed...SKIPPING"
+  else
+    echo "$i not installed need to install"
+    
+  fi
+
+
 done
