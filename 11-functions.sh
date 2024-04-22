403bbfe231502a4cd6120e3793af5e57e2c6fd1a
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-22 10:28:14 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,12 +5,12 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
-   if [ $2 -ne 0 ]
+   if [ $1 -ne 0 ]
     then
-      echo "$3 ...Failure"
+      echo "$2 ...Failure"
       exit 1
     else
-     echo "$3 ...Success"
+     echo "$2...Success"
    fi
 
 
