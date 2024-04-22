d642b282458f870fcde1894b14ae3be1e6a3795f
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-22 10:26:30 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
-   if [ $1 -ne 0 ]
+   if [ $? -ne 0 ]
     then
-      echo "$2 ...Failure"
+      echo "$? ...Failure"
       exit 1
     else
-     echo "$2 ...Success"
+     echo "$? ...Success"
    fi
 
 
